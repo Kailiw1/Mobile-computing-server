47a88e55dac6c16ee8e2835284cea12b73a4596e
daryldaryl <liboao1122@hotmail.com>
2017-08-24 01:44:50 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-// var express = require('express');
+var express = require('express');
 
-// var app = express();
-// var port = process.env.PORT || 1337;
-
-// app.get('/', function (req, res) {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     res.send('hello world??');
-// })
-// app.get('/test', function (req, res) {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     res.send('hello world????????');
-// })
-
-// app.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
-
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello 123");
-
-});
-
+var app = express();
 var port = process.env.PORT || 1337;
-server.listen(port);
+
+app.get('/', function (req, res) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    res.send('hello world??');
+})
+
+app.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
