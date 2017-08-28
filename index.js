916acaebbcc8aa5e1fe108c76ce5c18ba41eb340
daryldaryl <liboao1122@hotmail.com>
2017-08-28 15:36:18 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// var express = require('express');
+var express = require('express');
 
-// var app = express();
-// var port = process.env.PORT || 1337;
-
-// app.get('/', function (req, res) {
-//     res.writeHead(200, {"Content-Type": "text/plain"});
-//     res.send('hello world??');
-//     res.send('goodbye world')
-// })
-
-// app.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
-
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World??");
-
-});
-
+var app = express();
 var port = process.env.PORT || 1337;
-server.listen(port);
+
+app.get('/', function (req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.send('hello world??');
+    res.send('goodbye world')
+})
+
+app.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+// var http = require('http');
+
+// var server = http.createServer(function(request, response) {
+
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World??");
+
+// });
+
+// var port = process.env.PORT || 1337;
+// server.listen(port);
+
+// console.log("Server running at http://localhost:%d", port);
