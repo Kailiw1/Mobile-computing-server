94b08b039ab08759aa2757f33e922d9c6ffbe13c
daryldaryl <liboao1122@hotmail.com>
2017-08-28 19:45:10 +1000

    Update index.js

--- index.js
+++ index.js
@@ -5,23 +5,11 @@ var port = process.env.PORT || 1337;
 
 app.get('/', function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World????????");
+    res.end("Hello World??");
 })
 
 app.listen(port);
 
 console.log("Server is running", port);
 
-// var http = require('http');
 
-// var server = http.createServer(function(request, response) {
-
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World??");
-
-// });
-
-// var port = process.env.PORT || 1337;
-// server.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
