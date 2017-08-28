9430c1e5a74774ec31681f41b3b938b2d6662a98
daryldaryl <liboao1122@hotmail.com>
2017-08-28 19:54:16 +1000

    update index.js

--- index.js
+++ index.js
@@ -4,20 +4,14 @@ var app = express();
 var port = process.env.PORT || 1337;
 
 app.get('/', function (req, res) {
-<<<<<<< HEAD
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World????????");
-=======
-    res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("Hello World??");
->>>>>>> 94b08b039ab08759aa2757f33e922d9c6ffbe13c
 })
 
 app.get('/test', function (req, res) {
-    json = {
+    res.send({
         name: 'test'
-    }
-    res.send(json)
+    })
 })
 
 app.listen(port);
